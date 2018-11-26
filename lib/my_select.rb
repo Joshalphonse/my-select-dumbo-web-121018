b512b19ce8291210fb3df2275c79ffb4174559bf
Joshua Alphonse <joshalphonse@gmail.com>
2018-11-26 18:22:15 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,12 @@
 def my_select(collection)
  i = 0
- 
+ res = []
+ while i < collection.length
+ selection = yeild(collection[i])
+ if selection
+   res << collection[i]
+ end
+ i += 1
+end
+res
 end
