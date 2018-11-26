7688ad953631e9a58a56a67a4af473ec49218ba7
Joshua Alphonse <joshalphonse@gmail.com>
2018-11-26 18:23:35 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
 def my_select(collection)
  i = 0
- result = []
+ res = []
  while i < collection.length
    condition = yield(collection[i])
    if condition
-     result << collection[i]
+     res << collection[i]
    end
    i += 1
  end
- result
+ res
 end
